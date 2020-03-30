919e87a3407a2884e6237879d081a46568906e45
soniaArenas <puente46191@gmail.com>
2020-03-30 16:08:37 +0200

    First Commit

---
+++ EntityFramework/Context/ContextDb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFramework.Context
+{
+    public class ContextDb:DbContext
+    {
+        public ContextDb(DbContextOptions<ContextDb> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
